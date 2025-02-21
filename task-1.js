7aeedc6553b9ca74e5d8a35e1364edbfaefaf1fd
Lina <ulkopolina16@gmail.com>
2025-02-21 11:09:26 +0100

    Create task-1.js

---
+++ task-1.js
@@ -0,0 +1,9 @@
+<script type="module">
+  let name = 'Генератор захисного поля';
+  let price = 1000;
+
+  console.log(`Обрано «${name}», ціна за штуку ${price} кредитів`);
+
+  price = 2000;
+  console.log(`Обрано «${name}», ціна за штуку ${price} кредитів`);
+</script>
